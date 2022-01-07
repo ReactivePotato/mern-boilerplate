b1815572bc543d68f13c41296c8f2db2f5a2cd6e
ReactivePotato <potamatto@gmail.com>
2022-01-07 23:23:35 +1000

    added 2nd circle

--- client/components/d3linecircle.jsx
+++ client/components/d3linecircle.jsx
@@ -6,37 +6,42 @@ const DEFAULT_WIDTH = 400
 const DEFAULT_HEIGHT = 400
 
 const drawLine = ({ width, height }) => {
-  const scaleX = scaleLinear().domain([0, 20]).range([0, width])
-  const scaleY = scaleLinear().domain([0, 20]).range([0, height])
+  const scaleX = scaleLinear().domain([0, 30]).range([0, width])
+  const scaleY = scaleLinear().domain([0, 30]).range([0, height])
   const gX = select('#chart').append('g')
   const gY = select('#chart').append('g')
 
-  new Array(22).fill(0).forEach((it, index) => {
+  new Array(44).fill(0).forEach((it, index) => {
     gX.append('line')
       .attr('x1', scaleX(index))
       .attr('y1', 0)
       .attr('x2', scaleX(index))
-      .attr('y2', scaleY(20))
-      .attr('stroke', 'yellow')
+      .attr('y2', scaleY(40))
+      .attr('stroke', 'white')
       .attr('stroke-width', '1')
 
     gY.append('line')
       .attr('x1', scaleX(0))
       .attr('y1', scaleY(index))
-      .attr('x2', scaleX(20))
+      .attr('x2', scaleX(40))
       .attr('y2', scaleY(index))
-      .attr('stroke', 'yellow')
+      .attr('stroke', 'white')
       .attr('stroke-width', '1')
   })
 
   select('#chart')
     .append('circle')
-    .attr('cx', scaleX(5))
-    .attr('cy', scaleY(5))
-    .attr('r', 50)
-    .attr('stroke', 'red')
-    .attr('stroke-width', '2')
+    .attr('cx', scaleX(20))
+    .attr('cy', scaleY(20))
+    .attr('r', 30)
     .attr('fill', 'red')
+
+  select('#chart')
+    .append('circle')
+    .attr('cx', scaleX(10))
+    .attr('cy', scaleY(10))
+    .attr('r', 20)
+    .attr('fill', 'yellow')
 }
 
 const D3LineCircle = () => {
